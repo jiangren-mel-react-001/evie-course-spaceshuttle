b12ac6a85e36f400c9c36bc56be68ffed88580c7
Evie Ma <yfma89@gmail.com>
2018-02-07 13:31:07 +1100

    create major pages

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,55 @@
+
 import React, { Component } from 'react';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom'; 
+import { LinkContainer } from 'react-router-bootstrap';
+
+import { CoursesContainer } from './courses/courses-container';
+import { StudentsContainer } from './students/students-container';
+import { TeachersContainer } from './teachers/teachers-container';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h2>Hello world</h2>
-      </div>
+      <Router>
+        <div>
+          <Navbar inverse collapseOnSelect>
+            <Navbar.Header>
+              <Navbar.Brand>
+                Jiangren
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+              <Nav>
+                <LinkContainer to="/courses">
+                  <NavItem>Courses</NavItem>
+                </LinkContainer>
+                <LinkContainer to="/students">
+                  <NavItem>Students</NavItem>
+                </LinkContainer>
+                <LinkContainer to="/teachers">
+                  <NavItem>Teachers</NavItem>
+                </LinkContainer>
+              </Nav>
+              <Nav pullRight>
+                <NavItem eventKey={1} href="#">
+                  Login
+                </NavItem>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+          <Route exact path="/" component={CoursesContainer} />
+          <Route path="/courses" component={CoursesContainer} />
+          <Route path="/students" component={StudentsContainer} />
+          <Route path="/teachers" component={TeachersContainer} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/app/students/students-container.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const StudentsContainer = (props) => (
+    <h1>Students</h1>
+);
